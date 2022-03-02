f503923ed0b09bc493a0d1ac9e0202ba472ff276
danmainah <danielmainawaweru@gmail.com>
2022-03-02 13:23:20 +0300

    Add morehtml files and edge templating engine

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const path = require('path');
+
 const express = require('express');
 
 const app = new express();
@@ -7,6 +8,18 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+});
+
+app.get('/post', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
 });
 
 app.listen(4000, () => {
